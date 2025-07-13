2ec18f02fa19f532eaef196b11934de80b919701
Mrlík Vít <vit.mrlik@stapro.cz>
2025-07-13 17:00:52 +0200

    fix: relative asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : undefined,
+  base: mode === 'production' ? './' : undefined,
   server: {
     host: "::",
     port: 8080,
