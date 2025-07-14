465c6a7911722bffb00e3a98d1f046646a4163b4
Mrlík Vít <vit.mrlik@stapro.cz>
2025-07-14 08:25:08 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/vitmrlik.github.io/' : undefined,
+  base: '/',
   server: {
     host: "::",
     port: 8080,
